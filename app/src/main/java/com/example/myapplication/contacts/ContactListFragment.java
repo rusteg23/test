e5e711100c9e44613eca0866b209a36e47bb8c87
rusteg23 <rusteg23@gmail.com>
2018-12-02 19:58:30 +0400

    changed method onCreateView

--- app/src/main/java/com/example/myapplication/contacts/ContactListFragment.java
+++ app/src/main/java/com/example/myapplication/contacts/ContactListFragment.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -44,20 +46,20 @@ public class ContactListFragment extends Fragment implements ContactListContract
         }
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        View fragmentContactListView = inflater.inflate(R.layout.fragment_contact_list, container,
-                false);
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
-        contactsRV = (RecyclerView) fragmentContactListView.findViewById(R.id.contacts_recycler_view);
-        contactsRV.setLayoutManager(linearLayoutManager);
-
-        return fragmentContactListView;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_contact_list, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        contactsRV = (RecyclerView) view.findViewById(R.id.contacts_recycler_view);
+        contactsRV.setLayoutManager(linearLayoutManager);
+    }
 
     @Override
     public void onResume() {
