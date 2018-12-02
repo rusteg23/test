bd092e3754dc1f52e2a2ddea8ad35f9fe43ba494
rusteg23 <rusteg23@gmail.com>
2018-12-02 20:46:32 +0400

    added the annotation

--- app/src/main/java/com/example/myapplication/contacts/ContactListActivity.java
+++ app/src/main/java/com/example/myapplication/contacts/ContactListActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.contacts;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.myapplication.Injection;
@@ -17,8 +18,9 @@ public class ContactListActivity extends AppCompatActivity implements ContactLis
     private ContactDetailPresenter contactDetailPresenter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_contact);
 
         addContactListFragment();
